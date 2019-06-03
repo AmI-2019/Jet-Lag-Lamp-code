71276c26bf6ba1169c38ebac79344cdc41ecaa05
Bugliaa <a.bugliarelli@outlook.com>
2019-06-04 00:58:11 +0200

    Update the backend application... now works!

--- backend/ciao.py
+++ backend/ciao.py
@@ -1,13 +1,14 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-cred = credentials.Certificate("jet-lag-lamp-firebase-adminsdk-0rh7f-6066be5715.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("thejetlaglampapp-1-firebase-adminsdk-7oj9c-d795dc3925.json")
+default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-doc_ref = db.collection(u'Developers').document(u'uuuu')
-doc_ref.set({
-    u'name': "cello",
-    u'age': "21",
-})
+users_ref = db.collection(u'Users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
