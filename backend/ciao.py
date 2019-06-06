e5d5af150a6dcbe18fa4033bfda144f542f56f28
Bugliaa <a.bugliarelli@outlook.com>
2019-06-06 18:54:52 +0200

    Binding the application to the DATABASE

--- backend/ciao.py
+++ backend/ciao.py
@@ -1,14 +1,31 @@
 import firebase_admin
+from sleep_schedule import set_schedule
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 cred = credentials.Certificate("thejetlaglampapp-1-firebase-adminsdk-7oj9c-d795dc3925.json")
 default_app = firebase_admin.initialize_app(cred)
+schedule = {
+    u'sleep_time': u'22:00',
+    u'wake_time': u'07:00'
+}
 
 db = firestore.client()
 
 users_ref = db.collection(u'Users')
-docs = users_ref.stream()
+users = users_ref.stream()
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+for user in users:
+    # print(u'{} => {}'.format(user.id, user.to_dict()))
+    schedule_ref = db.collection(u'Users/{}/sleep_schedule'.format(user.id))
+    print(u'{} stays {} days abroad'.format(user.id, user.to_dict().get('trip_duration')))
+
+    dep_zone = user.to_dict().get('dep_zone')
+    arr_zone = user.to_dict().get('arr_zone')
+    print(dep_zone, arr_zone)
+    trip_duration = int(user.to_dict().get('trip_duration'))
+    set_schedule(dep_zone, arr_zone, trip_duration)
+    n = 0
+    while n < int(user.to_dict().get('trip_duration')):
+        schedule_ref.document(u'day_{}'.format(n)).set(schedule)
+        n += 1
--- backend/sleep_schedule.py
+++ backend/sleep_schedule.py
@@ -1,114 +1,115 @@
 from datetime import datetime, timedelta
 from pytz import timezone
 
+def set_schedule(dep_place, arr_place, days):
 
-myAppointments = [{"name": "wash the car", "start_time": "10/06/2019 09:00", "end_time": "10/06/2019 09:45"},
-                  {"name": "pet the hedgehog", "start_time": "10/06/2019 09:45", "end_time": "10/06/2019 10:00"},
-                  {"name": "meeting with Faustus", "start_time": "10/06/2019 11:30", "end_time": "10/06/2019 12:30"},
-                  {"name": "go to post office", "start_time": "10/06/2019 15:00", "end_time": "10/06/2019 16:00"},
-                  {"name": "dinner with Theresa", "start_time": "10/06/2019 19:15", "end_time": "10/06/2019 21:30"},
-                  {"name": "beer with Donald", "start_time": "10/06/2019 23:15", "end_time": "11/06/2019 0:30"}]
+    myAppointments = [{"name": "wash the car", "start_time": "10/06/2019 09:00", "end_time": "10/06/2019 09:45"},
+                      {"name": "pet the hedgehog", "start_time": "10/06/2019 09:45", "end_time": "10/06/2019 10:00"},
+                      {"name": "meeting with Faustus", "start_time": "10/06/2019 11:30", "end_time": "10/06/2019 12:30"},
+                      {"name": "go to post office", "start_time": "10/06/2019 15:00", "end_time": "10/06/2019 16:00"},
+                      {"name": "dinner with Theresa", "start_time": "10/06/2019 19:15", "end_time": "10/06/2019 21:30"},
+                      {"name": "beer with Donald", "start_time": "10/06/2019 23:15", "end_time": "11/06/2019 0:30"}]
 
-print("My appointments:")
+    print("My appointments:")
 
-# Defining home arrival time zones + UTC
-dep_zone = timezone('Europe/Rome')
-arr_zone = timezone('Asia/Singapore')
-utc = timezone('UTC')
+    # Defining home arrival time zones + UTC
+    dep_zone = timezone(dep_place)
+    arr_zone = timezone(arr_place)
+    utc = timezone('UTC')
 
-# Defining the sleeping time and the wake up time
-dep_sleep_time = datetime.strptime("08/06/2019 23:45", "%d/%m/%Y %H:%M")
-dep_sleep_time = dep_zone.localize(dep_sleep_time)
-dep_wake_time = datetime.strptime("09/06/2019 7:00", "%d/%m/%Y %H:%M")
-dep_wake_time = dep_zone.localize(dep_wake_time)
-utc_sleep_time = datetime.astimezone(dep_sleep_time, utc)
-utc_wake_time = datetime.astimezone(dep_wake_time, utc)
-arr_sleep_time = datetime.astimezone(dep_sleep_time, arr_zone)
-arr_wake_time = datetime.astimezone(dep_wake_time, arr_zone)
+    # Defining the sleeping time and the wake up time
+    dep_sleep_time = datetime.strptime("08/06/2019 23:45", "%d/%m/%Y %H:%M")
+    dep_sleep_time = dep_zone.localize(dep_sleep_time)
+    dep_wake_time = datetime.strptime("09/06/2019 7:00", "%d/%m/%Y %H:%M")
+    dep_wake_time = dep_zone.localize(dep_wake_time)
+    utc_sleep_time = datetime.astimezone(dep_sleep_time, utc)
+    utc_wake_time = datetime.astimezone(dep_wake_time, utc)
+    arr_sleep_time = datetime.astimezone(dep_sleep_time, arr_zone)
+    arr_wake_time = datetime.astimezone(dep_wake_time, arr_zone)
 
-# Other parameters and work variables
-days = 7
-direction = ''
-time_list = []
+    # Other parameters and work variables
+    # days = 7
+    direction = ''
+    time_list = []
 
-print("Home sleeping time: ", utc_sleep_time.strftime("%H:%M %Z"))
-print("Home wake up time: ", utc_wake_time.strftime("%H:%M %Z"))
+    print("Home sleeping time: ", utc_sleep_time.strftime("%H:%M %Z"))
+    print("Home wake up time: ", utc_wake_time.strftime("%H:%M %Z"))
 
-# Difference between the time zones
-diff_zone = datetime.utcoffset(arr_sleep_time) - datetime.utcoffset(dep_sleep_time)
-if diff_zone > timedelta(days=0, hours=0, minutes=0):
-    direction = 'east'
-else:
-    direction = 'west'
+    # Difference between the time zones
+    diff_zone = datetime.utcoffset(arr_sleep_time) - datetime.utcoffset(dep_sleep_time)
+    if diff_zone > timedelta(days=0, hours=0, minutes=0):
+        direction = 'east'
+    else:
+        direction = 'west'
 
-diff_zone = abs(diff_zone)
+    diff_zone = abs(diff_zone)
 
-# Computing a sleeping schedule without appointments on the calendar
-print("Suggested sleeping schedule:")
-print("Day 0 - Home:")
-print("Go to sleep at: ", dep_sleep_time.strftime("%H:%M"))
-print("Wake up at: ", dep_wake_time.strftime("%H:%M"), "\n")
+    # Computing a sleeping schedule without appointments on the calendar
+    print("Suggested sleeping schedule:")
+    print("Day 0 - Home:")
+    print("Go to sleep at: ", dep_sleep_time.strftime("%H:%M"))
+    print("Wake up at: ", dep_wake_time.strftime("%H:%M"), "\n")
 
-# i = timedelta(days=0, hours=0, minutes=0)
-i = 0
-c = 1
+    # i = timedelta(days=0, hours=0, minutes=0)
+    i = 0
+    c = 1
 
-# In the list I excluded day 0 (home)
-while i < days:
+    # In the list I excluded day 0 (home)
+    while i < days:
 
-    if direction == 'east':
-        arr_sleep_time = arr_sleep_time - diff_zone/days
-        arr_wake_time = arr_wake_time - diff_zone/days
-        myWake = arr_wake_time
-        mySleep = arr_sleep_time
-        for x in myAppointments:
-            start_time = datetime.strptime(x["start_time"], "%d/%m/%Y %H:%M")
-            start_time = arr_zone.localize(start_time)
-            if arr_sleep_time < start_time < arr_wake_time:
-                # Start the procedure for anticipating the alarm clock
-                # Set the alarm 1 hour before the beginning of the event
-                myWake = start_time - timedelta(hours=1)
-                mySleep = myWake - timedelta(hours=7)
-                break
+        if direction == 'east':
+            arr_sleep_time = arr_sleep_time - diff_zone/days
+            arr_wake_time = arr_wake_time - diff_zone/days
+            myWake = arr_wake_time
+            mySleep = arr_sleep_time
+            for x in myAppointments:
+                start_time = datetime.strptime(x["start_time"], "%d/%m/%Y %H:%M")
+                start_time = arr_zone.localize(start_time)
+                if arr_sleep_time < start_time < arr_wake_time:
+                    # Start the procedure for anticipating the alarm clock
+                    # Set the alarm 1 hour before the beginning of the event
+                    myWake = start_time - timedelta(hours=1)
+                    mySleep = myWake - timedelta(hours=7)
+                    break
 
-        print("Day %s:" % c)
-        print("Go to sleep at: ", mySleep.strftime("%d/%m/%Y %H:%M"))
-        print("Wake up at: ", myWake.strftime("%d/%m/%Y %H:%M"), "\n")
-        # print("Go to sleep at: ", arr_sleep_time.strftime("%d/%m/%Y %H:%M"))
-        # print("Wake up at: ", arr_wake_time.strftime("%d/%m/%Y %H:%M"), "\n")
-        time_list.append({"sleep_time": arr_sleep_time.strftime("%H:%M"), "wake:time": arr_wake_time.strftime("%H:%M")})
-        arr_sleep_time = arr_sleep_time + timedelta(days=1)
-        arr_wake_time = arr_wake_time + timedelta(days=1)
-        # i += timedelta(hours=1)
-        i += 1
-        c += 1
-    elif direction == 'west':
-        arr_sleep_time = arr_sleep_time + diff_zone/days
-        arr_wake_time = arr_wake_time + diff_zone/days
-        for x in myAppointments:
-            start_time = datetime.strptime(x["start_time"], "%d/%m/%Y %H:%M")
-            start_time = arr_zone.localize(start_time)
-            print(start_time)
-            if arr_sleep_time < start_time < arr_wake_time:
-                # Start the procedure for anticipating the alarm clock
-                # Set the alarm 1 hour before the beginning of the event
-                arr_wake_time = start_time - timedelta(hours=1)
-                break
+            print("Day %s:" % c)
+            print("Go to sleep at: ", mySleep.strftime("%d/%m/%Y %H:%M"))
+            print("Wake up at: ", myWake.strftime("%d/%m/%Y %H:%M"), "\n")
+            # print("Go to sleep at: ", arr_sleep_time.strftime("%d/%m/%Y %H:%M"))
+            # print("Wake up at: ", arr_wake_time.strftime("%d/%m/%Y %H:%M"), "\n")
+            time_list.append({"sleep_time": arr_sleep_time.strftime("%H:%M"), "wake:time": arr_wake_time.strftime("%H:%M")})
+            arr_sleep_time = arr_sleep_time + timedelta(days=1)
+            arr_wake_time = arr_wake_time + timedelta(days=1)
+            # i += timedelta(hours=1)
+            i += 1
+            c += 1
+        elif direction == 'west':
+            arr_sleep_time = arr_sleep_time + diff_zone/days
+            arr_wake_time = arr_wake_time + diff_zone/days
+            for x in myAppointments:
+                start_time = datetime.strptime(x["start_time"], "%d/%m/%Y %H:%M")
+                start_time = arr_zone.localize(start_time)
+                print(start_time)
+                if arr_sleep_time < start_time < arr_wake_time:
+                    # Start the procedure for anticipating the alarm clock
+                    # Set the alarm 1 hour before the beginning of the event
+                    arr_wake_time = start_time - timedelta(hours=1)
+                    break
 
-        print("Day %s:" % c)
-        print("Go to sleep at: ", arr_sleep_time.strftime("%d/%m/%Y %H:%M"))
-        print("Wake up at: ", arr_wake_time.strftime("%d/%m/%Y %H:%M"), "\n")
-        time_list.append({"sleep_time": arr_sleep_time.strftime("%H:%M"), "wake:time": arr_wake_time.strftime("%H:%M")})
-        arr_sleep_time = arr_sleep_time + timedelta(days=1)
-        arr_wake_time = arr_wake_time + timedelta(days=1)
-        # i += timedelta(days=1)
-        i += 1
-        c += 1
+            print("Day %s:" % c)
+            print("Go to sleep at: ", arr_sleep_time.strftime("%d/%m/%Y %H:%M"))
+            print("Wake up at: ", arr_wake_time.strftime("%d/%m/%Y %H:%M"), "\n")
+            time_list.append({"sleep_time": arr_sleep_time.strftime("%H:%M"), "wake:time": arr_wake_time.strftime("%H:%M")})
+            arr_sleep_time = arr_sleep_time + timedelta(days=1)
+            arr_wake_time = arr_wake_time + timedelta(days=1)
+            # i += timedelta(days=1)
+            i += 1
+            c += 1
 
 
-# Now it's time to adjust the schedule: read all the appointments
-# and set the alarm clock according to them
+    # Now it's time to adjust the schedule: read all the appointments
+    # and set the alarm clock according to them
 
-# Maybe it's better to add the date
-# Once I have defined the sleeping hours I can manage the interaction with the room
-# These include: Philips Hue, curtains, audio
+    # Maybe it's better to add the date
+    # Once I have defined the sleeping hours I can manage the interaction with the room
+    # These include: Philips Hue, curtains, audio
