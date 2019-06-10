d484e67dc908e56fcb43eeda5047c1d1df40f89e
Bugliaa <a.bugliarelli@outlook.com>
2019-06-10 15:37:05 +0200

    Added astral library for sunset/sunrise control

--- backend/room_control.py
+++ backend/room_control.py
@@ -1,6 +1,7 @@
 import firebase_admin
-from datetime import datetime
+from datetime import datetime, date
 from time import sleep
+from astral import Astral
 from pytz import timezone
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -12,7 +13,15 @@ db = firestore.client()
 
 users_ref = db.collection(u'Users')
 users = users_ref.stream()
+today = date.today()
+astral = Astral()
+print(astral.sunrise_utc(today, 45.06301, 7.66003))
+mz = timezone("Europe/Rome")
+mydate = datetime.astimezone(astral.sunrise_utc(today, 45.06301, 7.66003), mz)
 
+sunrise = datetime.astimezone(astral.sunrise_utc(today, 45.06301, 7.66003), mz)
+sunset = datetime.astimezone(astral.sunset_utc(today, 45.06301, 7.66003), mz)
+print(mydate)
 for user in users:
     schedule_ref = db.collection(u'Users/{}/sleep_schedule'.format(user.id))
     print(u'{} stays {} days abroad'.format(user.id, user.to_dict().get('trip_duration')))
@@ -21,9 +30,10 @@ for user in users:
     for schedule in time_schedule:
         wake_time = datetime.strptime(schedule.to_dict().get('wake_time'), "%d/%m/%Y %H:%M")
         sleep_time = datetime.strptime(schedule.to_dict().get('sleep_time'), "%d/%m/%Y %H:%M")
+        sleep_delta = wake_time - sleep_time
         # print("Sleep time: {}".format(sleep_time))
         # print("Wake time: {}".format(wake_time))
-        my_schedule.append({'sleep_time': sleep_time, 'wake_time': wake_time})
+        my_schedule.append({'sleep_time': sleep_time, 'wake_time': wake_time, 'sleep_delta': sleep_delta})
 
     n = 0
     for day in my_schedule:
@@ -31,8 +41,16 @@ for user in users:
         print("Sleep time: {}".format(datetime.strftime(day.get('sleep_time'), "%d/%m/%Y %H:%M")))
         print("Wake time: {}".format(datetime.strftime(day.get('wake_time'), "%d/%m/%Y %H:%M")))
         if day.get('sleep_time') < datetime.now() < day.get('wake_time'):
+            # The user is sleeping
             print("HELLO!!!")
             sleep(1)
+
+        if day.get('sleep_time') < datetime.now() < day.get('sleep_time') + day.get('sleep_delta')/2:
+            # The room must be DARK
+            print("HELLO DARKNESS")
+        elif day.get('sleep_time') + day.get('sleep_delta')/2 < datetime.now() < day.get('wake_time'):
+            # The room must be LIT, open the curtains 20-30 min before the sunset
+            print("HELLO LIGHT")
 
         n += 1
 
